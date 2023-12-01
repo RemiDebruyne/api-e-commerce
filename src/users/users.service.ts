192877cb01399b76fa2039cf9293a3505d18324d
RemiDebruyne <remidebruyne@hotmail.com>
2023-12-01 14:31:53 +0100

    feat(users-service): add logic response for create route of User controller

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { userInfo } from 'os';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private readonly prisma: PrismaService){}
+
+  public async create(createUserDto: CreateUserDto){
+    const createdUser = await this.prisma.users.create({
+      data: {
+        Pseudo: createUserDto.pseudo,
+        Mail: createUserDto.mail
+      },
+    });
+    return createUserDto
   }
+
 
   findAll() {
     return `This action returns all users`;
