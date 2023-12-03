b2dd55460f8c48c98c36482287f0851fb1a41b4b
RemiDebruyne <remidebruyne@hotmail.com>
2023-12-03 22:25:16 +0100

    feat(products): create route to get, update and delete product by UUID

--- src/products/product.controller.ts
+++ src/products/product.controller.ts
@@ -11,6 +11,8 @@ import {
 import { ProductService } from './product.service';
 import { ApiTags } from '@nestjs/swagger';
 import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateUserDto } from 'src/users/dto/update-user.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 
 @Controller('products')
@@ -20,5 +22,20 @@ export class ProductController {
   @Post()
   create(@Body() CreateProductDto: CreateProductDto){
     return this.productService.create(CreateProductDto)
+  }
+
+  @Get(':uuid')
+  public getByUUID(@Param('uuid') uuid: string ){
+    return this.productService.getByUUID(uuid);
+  }
+
+  @Patch(':uuid')
+  public updateByUUID(@Param('uuid') uuid: string, @Body() UpdateProductDto: UpdateProductDto) {
+    return this.productService.updateByUUID(uuid, UpdateProductDto );
+  }
+
+  @Delete(':uuid')
+  public deleteByUUID(@Param('uuid') uuid: string){
+    return this.productService.deleteByUUID(uuid);
   }
 }
--- src/products/product.service.ts
+++ src/products/product.service.ts
@@ -1,16 +1,17 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaService } from 'src/prisma.service';
 import NormalizedResponse from 'src/utils/normalized-response';
 
-
 @Injectable()
 export class ProductService {
-  constructor(private readonly prisma: PrismaService){}
-  
-  public async create(CreateProductDto: CreateProductDto){
+  constructor(private readonly prisma: PrismaService) {}
+
+  public async create(CreateProductDto: CreateProductDto) {
     const createdProduct = new NormalizedResponse(
-      `Product ${CreateProductDto.name} has been created`, await this.prisma.products.create({
+      `Product ${CreateProductDto.name} has been created`,
+      await this.prisma.products.create({
         data: {
           Name: CreateProductDto.name,
           Description: CreateProductDto.description,
@@ -19,11 +20,52 @@ export class ProductService {
           Author: {
             connect: {
               UUID: CreateProductDto.owner_uuid,
-            }
-          }
+            },
+          },
         },
       }),
     );
-      return createdProduct.toJSON();
+    return createdProduct.toJSON();
+  }
+
+  public async getByUUID(uuid: string) {
+    const gettedProduct = new NormalizedResponse(
+      `Product ${uuid} has been found`,
+      await this.prisma.products.findUnique({
+        where: {
+          UUID: uuid,
+        },
+      }),
+    );
+    return gettedProduct.toJSON();
+  }
+
+  public async updateByUUID(uuid: string, UpdateProductDto: UpdateProductDto) {
+    const updatedProduct = new NormalizedResponse(
+      `Product ${uuid} informations have been modified `,
+      await this.prisma.products.update({
+        where: {
+          UUID: uuid,
+        },
+        data: {
+          Name: UpdateProductDto.name,
+          Description: UpdateProductDto.description,
+          Price: UpdateProductDto.price,
+        },
+      }),
+    );
+    return updatedProduct.toJSON();
+  }
+
+  public async deleteByUUID(uuid: string, ) {
+    const deletedProduct = new NormalizedResponse(
+      `Product ${uuid} has been deleted`,
+      await this.prisma.products.delete({
+        where: {
+          UUID: uuid,
+        },
+      }),
+    );
+    return deletedProduct.toJSON();
   }
 }
